03a8f0760f67082b779188d8f69502678cce8eea
board2675 <board2675@gmail.com>
2019-07-10 12:18:48 +0300

    server almost works with register in fronted

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://orange-chat:orange-chat-password-that-is-so-simple@orange-chat-0-yhdz0.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret123"
-  };
+  mongoURI: "mongodb+srv://orange-chat:orange-chat-password-that-is-not-so-simple-@orange-chat-0-yhdz0.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "ngSUlq38onRZcxxbuB9aGFDIvYcaXp7F"
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -11,8 +12,10 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
+
 // Connect to MongoDB
 mongoose
   .connect(
@@ -24,10 +27,10 @@ mongoose
 
 // Passport middleware
 app.use(passport.initialize());
-// Passport config
 require("./config/passport")(passport);
+
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
