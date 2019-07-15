b51719a9a56862cdd659285b5f70c12ac2d36437
board2675 <board2675@gmail.com>
2019-07-15 11:56:31 +0300

    tring to make connection secure

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const auth = require("../../config/auth");
 
 // @route GET api/accounts/get
 // @desc return accounts types
@@ -11,8 +12,17 @@ const keys = require("../../config/keys");
 const Settings = require("../../models/Settings");
 
 router.get("/get", (req, res) => {
+  const userid = auth.token(req.headers['token']);
+  console.log(userid);
+  
+
+  
+  // let token = req.headers['token'] || req.headers['authorization'];
+
+
 
   Settings.findOne({ _id: "5d2b22ac1c9d4400006d66ef" }).then(accounts => {
+
     if (accounts) {
       return res.json(accounts)
     } else {
