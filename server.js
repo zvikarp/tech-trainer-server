5f91481322a1e28356b651e03ae945daef426033
board2675 <board2675@gmail.com>
2019-07-18 15:50:51 +0300

    ...

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ mongoose.Promise = global.Promise;
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", 'https://naughty-villani-d0f667.netlify.com');
 	res.header("Access-Control-Allow-Credentials", true);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header("Access-Control-Allow-Headers", '*');
 	next();
 });
 
