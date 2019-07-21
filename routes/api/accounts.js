81b078042f7894f5060236ae09b86a775fc7ade9
board2675 <board2675@gmail.com>
2019-07-21 16:56:40 +0300

    tring to make the admin able to update the accounts, having truble with it for some reason :(

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -6,6 +6,7 @@ const keys = require("../../config/keys");
 const userVerifier = require("../../config/userVerifier");
 const adminVerifier = require("../../config/adminVerifier");
 const Settings = require("../../models/Settings");
+const validateAccounts = require("../../validation/accounts");
 
 // @route GET api/accounts/get
 // @access Authed
@@ -35,14 +36,41 @@ router.post("/update", (req, routerRes) => {
 		if (!verifierRes.success) {
 			return routerRes.status(400).json(verifierRes);
 		}
-		Settings.findOne({ _id: "5d2b22ac1c9d4400006d66ef" }).then(accounts => {
-			if (accounts) {
+		// Settings.findOne({ _id: "5d2b22ac1c9d4400006d66ef" }).then(accounts => {
+			// if (accounts) {
+			// 	const recivedAccounts = req.body.accounts;
+			// 	Object.keys(recivedAccounts).forEach(accountId => {
+			// 		var oldAccount = accounts._doc[accountId];
+			// 		// console.log(recivedAccounts[accountId]);
+			// 		if (recivedAccounts[accountId].action === "new") oldAccount = recivedAccounts[accountId];
+			// 		// console.log(Object.keys(accounts._doc));
+					
+			// 		const { errors, isValid } = validateAccounts(oldAccount, recivedAccounts[accountId]);
+			// 		if (!isValid) {
+			// 			return res.status(400).json(errors);
+			// 		}
+			// 		delete recivedAccounts[accountId].action;
+			// 		accounts[accountId] = recivedAccounts[accountId];
+			// 		accounts[accountId].points = Number(recivedAccounts[accountId].points);
+			// 		console.log(accounts[accountId]);
+					
+					
+				// });
+				// console.log(2);
 				
-				return routerRes.json(accounts)
-			} else {
-				return routerRes.status(400).json({ accounts: "invalid request" });
-			}
-		});
+				Settings.findOneAndUpdate({ _id: "5d2b22ac1c9d4400006d66ef" }, { $set: { sdfsdfsdfsf: "yjfgh" } }, { upsert: true }).then(u => {
+					console.log(u);
+					
+			// 		console.log("all done!");
+			// 		return routerRes.json(accounts)
+
+				}).catch(err => {console.log(err);
+				});
+			// 	// console.log(accounts);
+			// } else {
+			// 	// return routerRes.status(400).json({ accounts: "invalid request" });
+			// }
+		// });
 	});
 });
 
---
+++ validation/accounts.js
@@ -0,0 +1,41 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+module.exports = function validateRegisterInput(oldData, newData) {
+	let errors = {};
+
+	newData.name = !isEmpty(newData.name) ? newData.name : "";
+	newData.points = !isEmpty(newData.points) ? newData.points : null;
+	newData.type = !isEmpty(newData.type) ? newData.type : null;
+	
+	switch (newData.action) {
+		case 'delete':
+			if (oldData.type === "website") {
+				errors.type = "You can't delete a website."
+			}
+			break;
+		case 'new':
+			if (oldData.type === "website") {
+				errors.action = "Can't create a new website."
+			}
+		default:
+			if (Validator.isEmpty(newData.name)) {
+				errors.name = "Name field is required";
+			}
+			if (!newData.points) {
+				errors.points = "points field is required";
+			}
+			if (Validator.isEmpty(newData.type)) {
+				errors.type = "Type field is required";
+			}
+			
+			if (oldData.type !== newData.type) {
+				errors.type = "Error with getting type";
+			}
+			break;
+	}
+	
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
