53a2a3ddbbfad6ce414bc1b9e2f186004ccd85ca
board2675 <board2675@gmail.com>
2019-07-24 12:09:31 +0300

    user changes the chart when he makes a change to his accounts

--- routes/api/cronjob.js
+++ routes/api/cronjob.js
@@ -57,6 +57,61 @@ function sortUsersByPoints(a, b) {
 	return 1;
 }
 
+async function getUsersPoints(user, accounts) {
+	var userPoints = {};
+	user.points = 0;
+	await asyncForEach(Object.keys(user.accounts), async (key) => {
+		if (!accounts[key]);
+		else if (accounts[key].type !== "website");
+		else if (user.accounts[key] == "");
+		else {
+			switch (accounts[key].name) {
+				case "GitHub":
+					const githubPoints = (await getGithubPoints(user.accounts[key]) * accounts[key].points);
+					user.points = user.points + githubPoints;
+					if (githubPoints > 0) {
+						userPoints.github = githubPoints;
+					}
+					break;
+				case "Medium":
+					const mediumPoints = (await getMediumPoints(user.accounts[key]) * accounts[key].points);
+					user.points = user.points + mediumPoints;
+					if (mediumPoints > 0) {
+						userPoints.medium = mediumPoints;
+					}
+					break;
+				case "Stackoverflow":
+					const stackoverflowPoints = (await getStackoverflowPoints(user.accounts[key]) * accounts[key].points);
+					user.points = user.points + stackoverflowPoints;
+					if (stackoverflowPoints > 0) {
+						userPoints.stackoverflow = stackoverflowPoints;
+					}
+					break;
+				default:
+					user.points += 0;
+					break;
+			}
+		}
+	});
+	await User.findOneAndUpdate(
+		{ _id: user.id },
+		{ $set: { points: user.points } }
+	).exec();
+	user.points += user.bonusPoints;
+	const userHistory = new History({
+		userId: user.id,
+		timestamp: Date.now(),
+		points: user.points,
+		accounts: userPoints,
+	});
+	await userHistory.save();
+	return {
+		'id': user.id,
+		'name': user.name,
+		'points': user.points
+	};
+}
+
 router.post("/updatepoints", async (req, routerRes) => {
 	//routerRes.setHeader('Access-Control-Allow-Origin', 'https://naughty-villani-d0f667.netlify.com');
 	userVerifier(req.headers["authorization"], verifierRes => {
@@ -78,65 +133,57 @@ router.post("/updatepoints", async (req, routerRes) => {
 					};
 					const accounts = settings.accounts;
 					await asyncForEach(users, async (user) => {
-						var userPoints = {};
-						user.points = 0;						
-						await asyncForEach(Object.keys(user.accounts), async (key) => {
-							if (!accounts[key]);
-							else if (accounts[key].type !== "website");
-							// if (accountsNames.indexOf(key) < 0);
-							else if (user.accounts[key] == "");
-							else {
-								switch (accounts[key].name) {
-									case "GitHub":
-										const githubPoints = (await getGithubPoints(user.accounts[key]) * accounts[key].points);
-										user.points = user.points + githubPoints;
-										if (githubPoints > 0) {
-											userPoints.github = githubPoints;
-										}
-										break;
-									case "Medium":
-										const mediumPoints = (await getMediumPoints(user.accounts[key]) * accounts[key].points);
-										user.points = user.points + mediumPoints;
-										if (mediumPoints > 0) {
-											userPoints.medium = mediumPoints;
-										}
-										break;
-									case "Stackoverflow":
-										const stackoverflowPoints = (await getStackoverflowPoints(user.accounts[key]) * accounts[key].points);
-										user.points = user.points + stackoverflowPoints;
-										if (stackoverflowPoints > 0) {
-											userPoints.stackoverflow = stackoverflowPoints;
-										}
-										break;
-									default:
-										user.points += 0;
-										break;
-								}
-							}
-						});
-						await User.findOneAndUpdate(
-							{ _id: user.id },
-							{ $set: { points: user.points } }
-						).exec();
-						user.points +=  user.bonusPoints;
-						const userHistory = new History({
-							userId: user.id,
-							timestamp: Date.now(),
-							points: user.points,
-							accounts: userPoints,
-						});
-						await userHistory.save();
-						userObject = {
-							'id': user.id,
-							'name': user.name,
-							'points': user.points
-						};
+						const userObject = await getUsersPoints(user, accounts);
 						if (user.points > passingPoints) {
 							chart.passed.push(userObject);
 						} else {
 							chart.under.push(userObject);
 						}
 					});
+					chart.passed.sort(sortUsersByPoints);
+					chart.under.sort(sortUsersByPoints);
+					chart.top3 = chart.passed.splice(0, 3);
+
+					await Chart.findOneAndUpdate({ _id: "5d2ed28f1c9d440000552aaa" }, { $set: { top3: chart.top3, passed: chart.passed, under: chart.under, lastUpdated: Date.now() } }, { upsert: true }).exec();
+
+					return routerRes.json({ test: 'success' });
+				});
+			});
+		});
+	});
+});
+
+router.post("/updateuserspoints", async (req, routerRes) => {
+	//routerRes.setHeader('Access-Control-Allow-Origin', 'https://naughty-villani-d0f667.netlify.com');
+	userVerifier(req.headers["authorization"], verifierRes => {
+		if (!verifierRes.success) {
+			return routerRes.status(400).json(verifierRes);
+		}
+		const uid = verifierRes.id;
+		return User.findOne({ _id: uid }).then(user => {
+			if (!user) return routerRes.status(400).json(messages.USER_NOT_FOUND_ERROR);
+			return Settings.findOne({ _id: "5d2b22ac1c9d4400006d66ef" }).then(settings => {
+				return Chart.findOne({ _id: "5d2ed28f1c9d440000552aaa" }).then(async (recivedChart) => {
+					if (!settings) return routerRes.status(400).json(messages.ACCOUNTS_NOT_FOUND);
+					const passingPoints = 50;
+					var chart = {
+						'top3': [],
+						'passed': [...recivedChart.top3, ...recivedChart.passed],
+						'under': recivedChart.under,
+					};
+					chart.passed = chart.passed.filter(function (child) {
+						return child.id !== uid;
+					});
+					chart.under = chart.under.filter(function (child) {
+						return child.id !== uid;
+					});
+					const accounts = settings.accounts;
+					const userObject = await getUsersPoints(user, accounts);
+					if (user.points > passingPoints) {
+						chart.passed.push(userObject);
+					} else {
+						chart.under.push(userObject);
+					}
 					chart.passed.sort(sortUsersByPoints);
 					chart.under.sort(sortUsersByPoints);
 					chart.top3 = chart.passed.splice(0, 3);
