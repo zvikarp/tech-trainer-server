dcbf0ebace1f37d0498d43a58fdb05762dfc3d37
ZviKarp <zvi@zektec.com>
2019-08-01 18:05:44 +0300

    i wasnt thinking...

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", config.origin);
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header('Access-Control-Allow-Methods', '*');
-	res.header('Access-Control-Allow-Methods', '*');
+	res.header("Access-Control-Allow-Headers", '*');
 	next();
 });
 
